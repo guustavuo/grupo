936c3928b5f9e964687eab3770481e65f52c1146
gustavo <145566459+guustavuo@users.noreply.github.com>
2024-09-10 08:39:17 -0300

    Add files via upload

---
+++ otica/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './componentes/Header'
+import SecaoCapa from './componentes/SecaoCapa'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <SecaoCapa/>
+    </div>
+  );
+}
+
+export default App;
---
+++ otica/src/componentes/SecaoCapa/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../SecaoCapa/style.css'
+
+
+const SecaoCapa = () => {
+    return (
+        <section className='secao-capa'>
+            <div className='capa center'>
+                <div className='texto-capa'>
+                    <p>Preços baixos em</p>
+                    <h1>Óculos de sol e de grau</h1>
+                    <p>Você só encontra aqui</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default SecaoCapa;
